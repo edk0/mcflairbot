349df9b6dbc50f1072ff564378307f7501244195
Ed Kellett <edk141@gmail.com>
2014-07-26 15:02:40 +0100

    use Flask-Migrate

--- manage.py
+++ manage.py
@@ -1,8 +1,12 @@
 from flask.ext.script import Manager
+from flask.ext.migrate import Migrate, MigrateCommand
 
-from flairbot.app import app
+from flairbot.app import app, db
 
 manager = Manager(app)
+migrate = Migrate(app, db)
+
+manager.add_command('db', MigrateCommand)
 
 if __name__ == "__main__":
     manager.run()
