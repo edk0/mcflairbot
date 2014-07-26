48d1c7d2ef90638eb3ec34dada89854ea7a1bb04
Ed Kellett <edk141@gmail.com>
2014-07-26 15:00:06 +0100

    flairbot/app: attempt to use pymysql (but fail silently if it's not found)

--- flairbot/app.py
+++ flairbot/app.py
@@ -2,6 +2,12 @@ from flask import Flask
 from flask.ext.cache import Cache
 from flask.ext.sqlalchemy import SQLAlchemy
 
+try:
+    import pymysql
+    pymysql.install_as_MySQLdb()
+except ImportError:
+    pass
+
 app = Flask(__name__, instance_relative_config=True)
 app.config.from_pyfile('default.cfg')
 app.config.from_pyfile('config.cfg', silent=True)
