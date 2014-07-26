01fe5761d32204549a832db8d674595134b50a74
Ed Kellett <edk141@gmail.com>
2014-07-26 14:59:34 +0100

    flairbot/app: use Flask-Cache

--- flairbot/app.py
+++ flairbot/app.py
@@ -1,9 +1,12 @@
 from flask import Flask
+from flask.ext.cache import Cache
 from flask.ext.sqlalchemy import SQLAlchemy
 
 app = Flask(__name__, instance_relative_config=True)
 app.config.from_pyfile('default.cfg')
 app.config.from_pyfile('config.cfg', silent=True)
+
+cache = Cache(app)
 
 db = SQLAlchemy(app)
 
