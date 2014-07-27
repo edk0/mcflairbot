cc1d4b1c39ccbebb3add7a4bad816de2e9770609
Ed Kellett <edk141@gmail.com>
2014-07-27 18:53:32 +0100

    use flask-admin

--- flairbot/app.py
+++ flairbot/app.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask.ext.admin import Admin
 from flask.ext.cache import Cache
 from flask.ext.sqlalchemy import SQLAlchemy
 
@@ -11,6 +12,8 @@ except ImportError:
 app = Flask(__name__, instance_relative_config=True)
 app.config.from_pyfile('default.cfg')
 app.config.from_pyfile('config.cfg', silent=True)
+
+admin = Admin(app)
 
 cache = Cache(app)
 
